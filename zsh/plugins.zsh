443945376c2e19be9a836d702b0b15ed47789999
Ayoub <jounaidayoub1@gmail.com>
2026-07-05 19:39:13 +0100

    Remove Oh My Zsh and manually load plugins for under 100ms speed

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -6,16 +6,7 @@ export YSU_MESSAGE_POSITION="after"
 # Starship prompt
 eval "$(starship init zsh)"
 
-# Shell completions (cached)
-if [ ! -f ~/.uv-completion.zsh ]; then
-  uv generate-shell-completion zsh > ~/.uv-completion.zsh 2>/dev/null
-fi
-[ -f ~/.uv-completion.zsh ] && source ~/.uv-completion.zsh
 
-if [ ! -f ~/.uvx-completion.zsh ]; then
-  uvx --generate-shell-completion zsh > ~/.uvx-completion.zsh 2>/dev/null
-fi
-[ -f ~/.uvx-completion.zsh ] && source ~/.uvx-completion.zsh
 
 # Eza completions
 export FPATH="$HOME/.eza/completions/zsh:$FPATH"
